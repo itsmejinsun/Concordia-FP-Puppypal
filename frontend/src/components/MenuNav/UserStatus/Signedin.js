c12d5fd78f1244511db050cbde71d7166305caa3
Jinsun Kim <itsmejinsun@gmail.com>
2021-10-07 11:01:13 -0400

    add style on loged in section

--- frontend/src/components/MenuNav/UserStatus/Signedin.js
+++ frontend/src/components/MenuNav/UserStatus/Signedin.js
@@ -8,16 +8,15 @@ const Signedin = ({ isSigninOpen }) => {
     const { user } = useAuth0();
     const { setIsPuppyListOpen } = useContext(PuppyContext);
 
+    const handleClick = () => {
+        setIsPuppyListOpen(true);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
         <Wrapper className={`${isSigninOpen}`}>
             <GreetTxt>Hi, {user.given_name}</GreetTxt>
-            <PuppyList
-                onClick={() => {
-                    setIsPuppyListOpen(true);
-                }}
-            >
-                Puppy List
-            </PuppyList>
+            <PuppyList onClick={handleClick}>Puppy List</PuppyList>
         </Wrapper>
     );
 };
@@ -46,6 +45,22 @@ const GreetTxt = styled.p`
     color: var(--button-color-secondary);
     font-size: 1.2rem;
 `;
-const PuppyList = styled.button``;
+const PuppyList = styled.button`
+    color: #fff;
+    background-color: var(--button-color-secondary);
+    border: none;
+    border-radius: 10px;
+    padding: 0.4rem 1.2rem;
+    margin-top: 0.5rem;
+    font-family: inherit;
+    letter-spacing: 0.5px;
+
+    &:hover,
+    &:focus {
+        outline: none;
+        animation: shake 1s;
+        transform-origin: center;
+    }
+`;
 
 export default Signedin;
